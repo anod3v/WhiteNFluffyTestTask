b7e4e41350d75f48910c53fc881b587f1c30a239
anod3v <anod3v@gmail.com>
2021-01-02 11:29:57 +0300

    storyboard removed

---
+++ WhiteNFluffyTestTask/WhiteNFluffyTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteNFluffyTestTask
+//
+//  Created by Andrey on 02/01/2021.
+//  Copyright © 2021 Andrey Anoshkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
